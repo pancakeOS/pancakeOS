83c3f69664df9cd6270d0180a86ce61fcb343396
Pancakse <pancakseyt@gmail.com>
2025-07-09 21:36:36 -0600

    do the basic of initializing duh

---
+++ main.lua
@@ -0,0 +1,10 @@
+function love.load()
+    love.window.setMode(800, 600, {
+        resizable = true,
+        minwidth = 800,
+        minheight = 600,
+        highdpi = true
+    })
+
+    love.window.maximize()
+end
