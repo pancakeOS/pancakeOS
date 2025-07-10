2d9aa151faab5281d6e06574c684ef042bd224c9
Pancakse <pancakseyt@gmail.com>
2025-07-10 10:42:06 -0600

    add custom cursor

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+
+local cursorImage
+local customCursor
+local selectCursorImage
+local selectCursor
+local textCursorImage
+local textCursor
+
+-- Cursor states: "default", "select", "text" (dont suggest changing these lol)
+local cursorState = "default"
+
 function love.load()
     love.window.setMode(800, 600, {
         resizable = true,
@@ -7,4 +18,12 @@ function love.load()
     })
 
     love.window.maximize()
+
+    -- Load custom cursor images
+    cursorImage = love.image.newImageData('assets/cursor.png')
+    customCursor = love.mouse.newCursor(cursorImage, 0, 0)
+    selectCursorImage = love.image.newImageData('assets/cursor_select.png')
+    selectCursor = love.mouse.newCursor(selectCursorImage, 0, 0)
+
+    love.mouse.setCursor(customCursor)
 end
